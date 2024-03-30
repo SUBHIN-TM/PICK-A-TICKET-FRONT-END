e64185aca24a08093ad85b784061a363e17fb228
SUBHIN-TM <subhintm2018@gmail.com>
2024-03-30 11:33:58 +0530

    shows alighned

--- src/components/ShowTime.jsx
+++ src/components/ShowTime.jsx
@@ -30,7 +30,7 @@ const ShowTime = () => {
   return (
     <div className="bg-black p-11">
       <div>
-        <h1 className="text-white text-2xl my-8">SHOW TIME</h1>
+        <h1 className="text-white md:text-2xl text-lg  my-8">SHOW TIME</h1>
       </div>
       <div className="w-12/12 flex justify-around sm:text-lg text-sm sm:w-/12 lg:w-7/12 text-gray-500 ">
         {dates.map((date) => (
@@ -41,18 +41,18 @@ const ShowTime = () => {
       </div>
       <div className="">
         {shows.map((shows)=> (
-          <div className="text-white lg:flex  my-14 border border-gray-600 p-2" key={shows._id}>
-            <div className="sm:flex ">
-              <img className="" src={shows.image} alt="" />
-              <div className="p-4 font-bold ">
+          <div className="text-white lg:flex  my-14 border border-gray-600 p-8" key={shows._id}>
+            <div className="md:flex ">
+              <img className="m-auto md:m-0" src={shows.image} alt="" />
+              <div className="p-4 font-bold text-center md:text-start ">
                 <h1>{shows.name}</h1>
                 <h1>{shows.language}</h1>
                 <h1>{shows.duration}</h1>
               </div>
             </div>
-            <div className="grid grid-cols-2 sm:flex lg:ml-10 mt-4 lg:mt-0">
+            <div className="grid grid-cols-2 gap-2 sm:flex lg:ml-10 mt-4 lg:mt-0 ">
               {shows?.shows?.map((shws,index)=> (
-                <div key={index} className=" lg:mt-5 mt-1 border w-28 text-center p-1 m rounded-sm  hover:bg-white hover:text-black cursor-pointer sm:mr-2 lg:h-16 lg:mx-4">
+                <div key={index} className="m-auto  lg:m-0 lg:mt-5 mt-1 border w-28 text-center p-1 m rounded-sm  hover:bg-white hover:text-black cursor-pointer sm:mr-2 lg:h-16 lg:mx-4">
                   <span>{shws.screen}</span><br />
                   <span>{shws.time}</span>     
                   </div>
