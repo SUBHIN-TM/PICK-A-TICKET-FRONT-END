fb921f544b2c8937494376b3453345ced1cdb3db
SUBHIN-TM <subhintm2018@gmail.com>
2024-06-11 14:56:21 +0530

    tiket generator design pending

--- src/components/TicketGenerator.jsx
+++ src/components/TicketGenerator.jsx
@@ -1,8 +1,55 @@
-import React from 'react'
+import axios from 'axios'
+import  { useRef, useState } from 'react'
+import 'react-toastify/dist/ReactToastify.css'; // Import the CSS file for react-toastify
+import { ToastContainer ,toast} from 'react-toastify';
 
 const TicketGenerator = () => {
+
+  const idRef=useRef()
+  const[ticketDetails,setTicketDetails]=useState({})
+  const[isWait,setIsWait]=useState(false)
+  const generate= async()=>{
+   
+    try {
+      setIsWait(true)
+      const response=await axios.post('http://localhost:3000/ticketGenerator',{id:idRef.current.value})
+      setTicketDetails(response.data.details)
+    } catch (error) {
+      console.error(error);
+      toast.error(error.response?.data?.message || 'An error occurred');
+    }finally{
+      setIsWait(false)
+    }
+  
+  }
+
+  console.log(ticketDetails);
+
   return (
-    <div>TicketGenerator</div>
+    <div className='bg-black text-white h-[524px]'>
+    <div className='p-10'>
+      <label className='mr-3' htmlFor="ticketId">Ticket ID</label> <input ref={idRef}  className='w-56 text-black p-1' type="text" name="" id="" /> <span onClick={generate} className='ml-4 border p-1 rounded-md font-semibold'>Generate</span>
+    </div>
+    {isWait && (
+      <div>Loading...</div>
+    )}
+
+ {ticketDetails?.inner &&(
+   <div>
+    Name : {ticketDetails.inner.name} <br />
+    Mail : {ticketDetails.inner.mail} <br />
+    Mobile Number : {ticketDetails.inner.mobile} <br />
+    Number Of Booked Seats : {ticketDetails.inner.seatNumber.length} <br />
+    Seat Numbers : {`${ticketDetails.inner.seatNumber}`} <br />
+    Movie Name : {ticketDetails.main.movie} <br />
+    Date : {ticketDetails.main.date} <br />
+    Screen : {ticketDetails.main.screen} <br />
+    Time : {ticketDetails.main.time} <br />
+   </div>
+ )}
+    
+    <ToastContainer/>
+    </div>
   )
 }
 
